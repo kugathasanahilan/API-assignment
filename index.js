e46858fc2d9e53c362f16a8b6f41f5d226764680
kugathasanahilan <kugathasanahilan@gmail.com>
2021-09-27 01:55:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import 'dotenv/config';
+import saySomething from './my-other-file.js';
+console.log('Hello Node.js project.');
+console.log(process.env.MY_SECRET);
+console.log(saySomething);
---
+++ my-other-file.js
@@ -0,0 +1,3 @@
+export default function saySomething(MY_SECRET) {
+    MY_SECRET
+};
